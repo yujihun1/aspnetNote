0c71075c35a034e4fadd0ca5803ac23cc89ee45b
yujihun <sk0253642@gmail.com>
2025-05-13 14:41:14 +0900

    logout add

--- aspnetNote/Controllers/AccountController.cs
+++ aspnetNote/Controllers/AccountController.cs
@@ -33,6 +33,7 @@ namespace aspnetNote.Controllers
                     if (user != null)
                     {
                         //로그인 성공
+                        HttpContext.Session.SetInt32("USER_LOGIN_KEY",user.UserNo);
                         return RedirectToAction("LoginSuccess", "Home"); //로그인 성공 페이지로 이동
                     }
                 }
@@ -40,6 +41,12 @@ namespace aspnetNote.Controllers
                 ModelState.AddModelError(string.Empty, "사용자 ID 혹은 비밀번호가 올바르지 않습니다.");
             }
             return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("USER_LOGIN_KEY");
+            return RedirectToAction("Index", "Home");
         }
 
         
--- aspnetNote/Program.cs
+++ aspnetNote/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSession();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,7 +14,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
